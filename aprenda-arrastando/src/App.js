a3bb24a56ffaa60c5c9178a18c64c8b87ad21c17
Diogo <drd.rclrd@gmail.com>
2019-11-26 14:59:37 -0300

    new feature with style and other things

--- aprenda-arrastando/src/App.js
+++ aprenda-arrastando/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Routes from './routes';
-
+import GlobalStyle from './global'
 function App() {
   return (
     <>
       <Routes />
+      <GlobalStyle />
     </>
   );
 }
---
+++ aprenda-arrastando/src/components/atoms/container.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+`;
+
+export default Container;
