ac73fa415f5b014e9f505a3f1bab35f37599726b
Diogo <drd.rclrd@gmail.com>
2019-11-27 11:43:26 -0300

    new css :art:

--- aprenda-arrastando/src/components/pages/page_css/index.js
+++ aprenda-arrastando/src/components/pages/page_css/index.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Reference, Exercise, Options } from './styles_css';
+import {
+  Exercise,
+  Options,
+  StyledDiv,
+  StyledSection,
+  Row,
+  Footer,
+} from './styles_css';
 import {toast} from 'react-toastify';
+import Container from '../../atoms/container';
+import { H1, P, Ol, Li } from '../../atoms/text';
 
 export default function PageCss() {
-  const [style1, setStyle1] = useState(false);
+  const [shadowStyle1, setStyle1] = useState(false);
   const [style2, setStyle2] = useState(false);
   const [style3, setStyle3] = useState(false);
   const [style4, setStyle4] = useState(false);
@@ -12,7 +21,7 @@ export default function PageCss() {
 
   
   function handleStyle1() {
-    setStyle1(!style1);
+    setStyle1(!shadowStyle1);
   }
   function handleStyle2() {
     setStyle2(!style2);
@@ -33,50 +42,53 @@ export default function PageCss() {
   }
 
   useEffect(() => {
-    if (style1 && style2 && style3 && style4 && !style5 && !style6){
+    if (shadowStyle1 && style2 && style3 && style4 && !style5 && !style6){
       toast.success("Parabéns, pode ir para o próximo nível!");
     };
   });
 
   return (
     <Container>
-      <Reference>
-      <h1>RSXP</h1>
-      <p>Viva uma experiência única!</p>
-      <ul>
-          <li>foco no aprendizado</li>
-          <li>foco na prática</li>
-          <li>foco no resultado</li>
-      </ul>
-      <footer>Credenciamento às 08:00</footer>
-    </Reference>
+      <StyledSection>
+      <Row>
+        <StyledDiv margin padding shadow borderRadius width height>
+          <H1>RSXP</H1>
+          <P>Viva uma experiência única!</P>
+          <Ol>
+            <Li>foco no aprendizado</Li>
+            <Li>foco na prática</Li>
+            <Li>foco no resultado</Li>
+          </Ol>
+          <Footer>Credenciamento às 08:00</Footer>
+        </StyledDiv>
+        <Exercise 
+          shadowStyle1={shadowStyle1} 
+          style2={style2}
+          style3={style3}
+          style4={style4}
+          style5={style5}
+          style6={style6}>
+          <h1>RSXP</h1>
+          <p>Viva uma experiência única!</p>
+          <ul>
+              <Li>foco no aprendizado</Li>
+              <Li>foco na prática</Li>
+              <Li>foco no resultado</Li>
+          </ul>
+          <footer>Credenciamento às 08:00</footer>
+        </Exercise>
+      </Row>
+      <Options>
+        <button className="teste" oi={shadowStyle1} onClick={handleStyle1}>background-color</button>
+        <button onClick={handleStyle2}>color</button>
+        <button onClick={handleStyle3}>font-family</button>
+        <button onClick={handleStyle4}>align-text</button>
 
-    <Exercise 
-      style1={style1} 
-      style2={style2}
-      style3={style3}
-      style4={style4}
-      style5={style5}
-      style6={style6}>
-      <h1>RSXP</h1>
-      <p>Viva uma experiência única!</p>
-      <ul>
-          <li>foco no aprendizado</li>
-          <li>foco na prática</li>
-          <li>foco no resultado</li>
-      </ul>
-      <footer>Credenciamento às 08:00</footer>
-    </Exercise>
-    <Options>
-      <button className="teste" oi={style1} onClick={handleStyle1}>background-color</button>
-      <button onClick={handleStyle2}>color</button>
-      <button onClick={handleStyle3}>font-family</button>
-      <button onClick={handleStyle4}>align-text</button>
-
-      <button onClick={handleStyle5}>font-style</button>
-      <button onClick={handleStyle6}>text-transform</button>
-      
-    </Options>
+        <button onClick={handleStyle5}>font-style</button>
+        <button onClick={handleStyle6}>text-transform</button>
+        
+      </Options>
+      </StyledSection>
     </Container>
     
   )
