8d808fa7a4ec0f7678563454cde23d87251b93b5
Carlos Oliveira <54965836+burn-c@users.noreply.github.com>
2019-11-27 13:00:50 -0300

    Update styles_css.js

--- aprenda-arrastando/src/components/pages/page_css/styles_css.js
+++ aprenda-arrastando/src/components/pages/page_css/styles_css.js
@@ -5,26 +5,40 @@ export const Reference = styled.div`
   background-color: black;
   color: white;
   font-family: "Arial";
-  text-align: center;
+  text-align: left;
 `;
 
 export const Exercise = styled.div`
-  background-color: ${props => (props.style1 ? "black" : "none")};
-  color: ${props => (props.style2 && "gray" )};
-  font-family: ${props => (props.style3 ? "Arial" : "Times")};
-  text-align: ${props => (props.style4 ? "center" : "left")};
-  font-style: ${props => (props.style5 ? "italic" : "normal")};
-  text-transform: ${props => (props.style6 ? "uppercase" : "none")};
 
-  box-shadow: ${({ shadowStyle1 }) => ( shadowStyle1 && '0px 0px 15px 0px #888888' )};
-  color: gray;
+font-family: 'time';
+/*   text-align: ${props => (props.style4 ? "center" : "left")};
+  font-style: ${props => (props.style5 ? "italic" : "normal")};
+  text-transform: ${props => (props.style6 ? "uppercase" : "none")}; */
+
+  
 
   /* ALTERAR COM ESSA PROPRIEDADES */
-  /* margin: ${({ margin }) => (margin && '10px')};
-  padding: ${({ padding }) => (padding && '20px')};
-  border-radius: ${({ borderRadius }) => ( borderRadius && '5px')} ;
-  width: ${({ width }) => (width && '40%')};
-  height: ${({ height }) => (height && '35vh')}; */
+  box-shadow: ${({ shadowStyle1 }) => ( shadowStyle1 && '0px 0px 15px 0px #888888' )};
+  background-color: ${({ shadowStyle1 }) => (shadowStyle1 && 'black')}; /* ${shadowStyle1 => (shadowStyle1 ? "" : "black")};  */
+  margin: ${({ shadowStyle1 }) => (shadowStyle1 && '10px')};
+  padding: ${({ shadowStyle1 }) => (shadowStyle1 && '20px')};
+  border-radius: ${({ shadowStyle1 }) => ( shadowStyle1 && '5px')} ;
+  width: ${({ shadowStyle1 }) => (shadowStyle1 && '40%')};
+  height: ${({ shadowStyle1 }) => (shadowStyle1 && '35vh')}; 
+
+
+  color: ${({ colorStyle2 }) => (colorStyle2 && '#fff')}; 
+
+
+  font-family: ${({ fontStyle3 }) => (fontStyle3 && 'Arial')};
+
+  text-align: ${({ fontAlignStyle4 }) => (fontAlignStyle4 && 'center')}; /* ${props => (props.style4 ? "center" : "left")}; */
+  
+  font-style: ${({ fontStyleStyle5 }) => (fontStyleStyle5 && 'normal')};  /* ${props => (props.style5 ? "italic" : "normal")}; */
+
+
+
+
 `;
 
 export const Options = styled.div`
@@ -34,23 +48,25 @@ export const Options = styled.div`
 `;
 
 export const Btn1 = styled(Button)`
-  background-color: ${props => (props.style1 ? "green" : "default")};
-  color: ${props => (props.style1 ? "white" : "default")};
+  background-color: ${props => (props.shadowStyle1 ? "green" : "default")};
+  color: ${props => (props.shadowStyle1 ? "white" : "default")};
 `;
 
 export const Btn2 = styled(Button)`
-  background-color: ${props => (props.style2 ? "green" : "default")};
-  color: ${props => (props.style2 ? "white" : "default")};
+  background-color: ${props => (props.colorStyle2 ? "green" : "default")};
+  color: ${props => (props.colorStyle2 ? "white" : "default")};
 `;
 
 export const Btn3 = styled(Button)`
-  background-color: ${props => (props.style3 ? "green" : "default")};
-  color: ${props => (props.style3 ? "white" : "default")};
+  background-color: ${props => (props.fontStyle3 ? "green" : "default")};
+  color: ${props => (props.fontStyle3 ? "white" : "default")};
 `;
 
 export const Btn4 = styled(Button)`
-  background-color: ${props => (props.style4 ? "green" : "default")};
-  color: ${props => (props.style4 ? "white" : "default")};
+  background-color: ${props => (props.fontAlignStyle4 ? "green" : "default")};
+  color: ${props => (props.fontAlignStyle4 ? "white" : "default")};
+  /* background-color: ${fontAlignStyle4 => (fontAlignStyle4 ? "green" : "default")};
+  color: ${fontAlignStyle4 => (fontAlignStyle4 ? "white" : "default")}; */
 `;
 
 export const Btn5 = styled(Button)`
@@ -68,10 +84,12 @@ margin: ${({ margin }) => (margin && '10px')};
 padding: ${({ padding }) => (padding && '20px')};
 box-shadow: ${({ shadow }) => ( shadow && '0px 0px 15px 0px #888888' )};
 border-radius: ${({ borderRadius }) => ( borderRadius && '5px')} ;
-color: gray;
+color: white;
 width: ${({ width }) => (width && '40%')};
 height: ${({ height }) => (height && '35vh')};
-`;
+background-color:#000;
+text-align: center;
+  `;
 
 export const StyledSection = styled.section`
 width: 90%;
@@ -95,3 +113,5 @@ export const Row = styled.div`
 export const Footer = styled.footer`
   padding: 40px 0 0 10px;
 `;
+
+
